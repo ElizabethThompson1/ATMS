5e017f6f8223d5569d9611d6ba1913f3d374443d
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-11-02 09:55:03 -0500

    changes the veriables to let insted of const.

--- account.js
+++ account.js
@@ -1,8 +1,6 @@
 "use strict";
 //! START HERE! Then atm.js, then index.js
-const {balance, takeOutMoney, addMoney, correctPin}= require("./atm");
-const {mainMenu, accessATM, } = require("./index.js")
-const prompt = require("prompt-sync")
+let prompt = require("prompt-sync");
 
 
 
@@ -10,21 +8,16 @@ const prompt = require("prompt-sync")
 
 //TODO: Create variables for account balance & account pin
 
-function atmPin() {
-    let accountpin = 1123
-    return accountpin
-}
+const pinFunction = 1123
 
-let accountBalance ="10,ooo"
-function atmBalance (){
-   
-    console.log(accountBalance)
-}
+
+
+let atmBalance ="10,ooo"
 
 //Export these variables
 
 module.export = {
-    pin: atmPin,
+    pin: pinFunction,
     fullBalance: atmBalance,
   };
 
--- index.js
+++ index.js
@@ -27,28 +27,27 @@ function mainMenu() {
         for (let i = 0; i< arr.length; i++){
         finalOption += arr[i] + "\n";
         }
-  let questionarray =console.log(require('prompt-sync')()( "what would you like to do?"+ " "+finalOption))      
-        switch (questionarray) {
-          case "1","1.Current Balance","Current Balance":
-            balance()
-            break;
-          case "2","2.Make A Deposit","Make A Deposit":
-            addMoney()
-            break;
-          case "3","3.Make a Withdrawal","Make a Withdrawal":
-            takeOutMoney()
-            break;
-          case "4","4.restart","restart":
-            mainMenu()
-            break;
-          case "quit":
-            accessATM()
-          default:
-            return mainMenu(); // ask again
-        }
-  //TODO: Set up a main menu.  Prompt users for ATM selection to do the following:
-  //! Remember - we should keep prompting the user for options until they quit!
+  let questionarray =console.log(require('prompt-sync')()( "Enter" +" "+finalOption))      
+        if (questionarray ==="1" || questionarray ==="1.Current Balance"|| questionarray==="Current Balance") {
+            console.log(balance)}
+          else if(questionarray=== "2" || questionarray ==="2.Make A Deposit"|| questionarray==="Make A Deposit"){
+            console.log(addMoney)}
+            
+          else if(questionarray ==="3"|| questionarray==="3.Make a Withdrawal"||questionarray ==="Make a Withdrawal"){
+            console.log(takeOutMoney)}
+          else if (questionarray ==="4"|| questionarray ==="4.restart"|| questionarray ==="restart"){
+            mainMenu()}
+          else if (questionarray ==="quit"){
+            accessATM()}
+          else{
+            return mainMenu(); }
+          
 
+          //TODO: Set up a main menu.  Prompt users for ATM selection to do the following:
+         
 }
+
+
+//! Remember - we should keep prompting the user for options until they quit
 module.export = { mainMenu,accessATM
 };
